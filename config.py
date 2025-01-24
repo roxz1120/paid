6d852905d452ac3b114f5089932c06349b7e6a8d
roxz1120 <anahulmotwani@outlook.com>
2025-01-25 01:17:09 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8078932996:AAGd4u-cX96yInbNHD_QCJZ56gZ7a9Lqt9c")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Mk_ddos")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7353797869")
+
+
+
+
+
+
+
+  
