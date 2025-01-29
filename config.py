fb8fe8a4ae70c5541ef856b7a373a2ce0d2360d8
roxz1120 <anahulmotwani@outlook.com>
2025-01-29 11:43:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BOT_TOKEN=("8078932996:AAGd4u-cX96yInbNHD_QCJZ56gZ7a9Lqt9c")
 OWNER_USERNAME=("@Mk_ddos")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("7353797869")
+ADMIN_IDS=("7353797869","7352008650")
 
 
 
